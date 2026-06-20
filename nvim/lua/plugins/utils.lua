fc82b93871777536eb6c42e09f0b0f5104128a6f
MaiteALC <maitexp549@gmail.com>
2026-06-20 20:10:35 -0300

    refactor(nvim): update which-key groups
    - Add 'Find' and 'Goto' groups
    - Remove the 'LSP Actions' kickstart-provided group

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -20,7 +20,7 @@ require('which-key').setup {
     { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
     { '<leader>t', group = '[T]oggle' },
     { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
-    { 'gr', group = 'LSP Actions', mode = { 'n' } },
+    { '<leader>f', group = '[F]ind' },
+    { '<leader>g', group = '[G]oto' },
   },
 }
-
