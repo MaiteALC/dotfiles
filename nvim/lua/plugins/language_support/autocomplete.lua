2f995affd5e4c322155a5873679afaec54332700
MaiteALC <maitexp549@gmail.com>
2026-06-20 15:17:50 -0300

    feat(nvim): use the Rust-based blink.cmp fuzzy matcher
    Set the `fuzzy.implementation` to `prefer_rust_with_warning`.
    With this approach, the Rust implementation will be used by default.
    If it is not available, a warning will be displayed and the Lua implementation will be used.

--- nvim/lua/plugins/language_support/autocomplete.lua
+++ nvim/lua/plugins/language_support/autocomplete.lua
@@ -42,14 +42,7 @@ require('blink.cmp').setup {
 
   snippets = { preset = 'luasnip' },
 
-  -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-  -- which automatically downloads a prebuilt binary when enabled.
-  --
-  -- By default, we use the Lua implementation instead, but you may enable
-  -- the rust implementation via `'prefer_rust_with_warning'`
-  --
-  -- See `:help blink-cmp-config-fuzzy` for more information
-  fuzzy = { implementation = 'lua' },
+  fuzzy = { implementation = 'prefer_rust_with_warning' },
 
   -- Shows a signature help window while you type arguments for a function
   signature = {
