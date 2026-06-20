0018b1d52df4396aa5e87f969736a1a6f01f7409
MaiteALC <maitexp549@gmail.com>
2026-06-20 20:27:09 -0300

    style(nvim): reorganize keymap declarations

--- nvim/lua/plugins/language_support/debug.lua
+++ nvim/lua/plugins/language_support/debug.lua
@@ -6,13 +6,13 @@ vim.pack.add {
   'https://github.com/jay-babu/mason-nvim-dap.nvim',
 }
 
--- Basic debugging keymaps, feel free to change to your liking!
-vim.keymap.set('n', '<F5>', function() require('dap').continue() end, { desc = 'Debug: Start/Continue' })
+vim.keymap.set('n', '<leader>tb', function() require('dap').toggle_breakpoint() end, { desc = 'Debug: [T]oggle [B]reakpoint' })
+vim.keymap.set('n', '<leader>cb', function() require('dap').set_breakpoint(vim.fn.input 'Conditional Breakpoint: ') end, { desc = 'Debug: Set Breakpoint' })
+
 vim.keymap.set('n', '<F1>', function() require('dap').step_into() end, { desc = 'Debug: Step Into' })
 vim.keymap.set('n', '<F2>', function() require('dap').step_over() end, { desc = 'Debug: Step Over' })
 vim.keymap.set('n', '<F3>', function() require('dap').step_out() end, { desc = 'Debug: Step Out' })
-vim.keymap.set('n', '<leader>tb', function() require('dap').toggle_breakpoint() end, { desc = 'Debug: [T]oggle [B]reakpoint' })
-vim.keymap.set('n', '<leader>cb', function() require('dap').set_breakpoint(vim.fn.input 'Conditional Breakpoint: ') end, { desc = 'Debug: Set Breakpoint' })
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end, { desc = 'Debug: Start/Continue' })
 
 -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
 vim.keymap.set('n', '<F7>', function() require('dapui').toggle() end, { desc = 'Debug: See last session result.' })
