40702b9916ebfa5174259c3b77af77905e7c169f
MaiteALC <maitexp549@gmail.com>
2026-06-24 16:31:13 -0300

    feat(nvim): integrate neo-tree with snacks.rename
    Add event handlers to call the appropriate Snacks rename file function and trigger LSP workspace edits.

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -6,8 +6,18 @@ vim.pack.add {
 
 vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
 
+local function on_rename(data) Snacks.rename.on_rename_file(data.source, data.destination) end
+
+local events = require 'neo-tree.events'
+
 require('neo-tree').setup {
+  event_handlers = {
+    { event = events.FILE_RENAMED, handler = on_rename },
+    { event = events.FILE_MOVED, handler = on_rename },
+  },
+
   filesystem = {
+    use_libuv_file_watcher = true,
     window = {
       mappings = {
         ['\\'] = 'close_window',
