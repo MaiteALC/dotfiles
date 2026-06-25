3487ad98fad25e4742a951cd9e0c62d8cc087b0f
MaiteALC <maitexp549@gmail.com>
2026-06-25 16:41:44 -0300

    docs(nvim): add attribution to kickstart in init.lua
    Add a header comment in the main entry file to acknowledge `kickstart.nvim` as the foundational template for this configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,11 @@
+-- =====================================================================
+-- Neovim Configuration
+--
+-- This configuration was originally based on `kickstart.nvim`
+-- (https://github.com/nvim-lua/kickstart.nvim) and heavily customized
+-- for a modular architecture.
+-- =====================================================================
+
 require 'core'
 require 'theme'
 require 'plugins'
