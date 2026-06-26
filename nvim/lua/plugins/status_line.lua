565bbc9d3712dc0bc95d7c5fd6f926c34af11595
MaiteALC <maitexp549@gmail.com>
2026-06-26 16:50:08 -0300

    refactor(nvim): tweaked padding of fileformat lualine component

--- nvim/lua/plugins/status_line.lua
+++ nvim/lua/plugins/status_line.lua
@@ -171,7 +171,7 @@ require('lualine').setup {
     lualine_x = {
       {
         'fileformat',
-        padding = { left = 0, right = 0 },
+        padding = { left = 0, right = 1 },
       },
       {
         'filetype',
