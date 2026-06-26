643d31388f1f413e5ff4ca38885c090a4b68e77f
MaiteALC <maitexp549@gmail.com>
2026-06-26 20:03:54 -0300

    refactor(hypr): refine glassmorphism effect
    Tweak blur properties to achieve a "tinted glass aesthetic" and improve readability in windows with transparency enabled.
    - Increase blur size
    - Decrease blur vibrancy, noise and brightness

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -65,17 +65,17 @@ hl.config({
         blur = {
             enabled = true,
 
-            size = 2,
+            size = 3,
             passes = 3,
-            vibrancy = 0.3,
+            vibrancy = 0.2,
 
             new_optimizations = true,
             ignore_opacity = true,
             xray = true,
 
-            noise = 0.03,
+            noise = 0.02,
             contrast = 1,
-            brightness = 0.82,
+            brightness = 0.56,
 
             popups = true,
             special = true,
