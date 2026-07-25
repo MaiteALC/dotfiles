14d6e75cf05e7cd5aa8846a9f2df8ecce2d6d51a
MaiteALC <maitexp549@gmail.com>
2026-07-25 14:33:55 -0300

    feat(zsh): add custom Zsh aliases
    Create `aliases.zsh` file to store all the shell's aliases. This file is
    intended to be sourced by `.zshrc`.

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,23 @@
+# --- Utilities ---
+alias c=clear
+
+alias history='history -i'
+
+alias diff='diff --color=auto'
+
+alias rgrep='rg --color=auto'
+# -----------------
+
+# --- Eza (better ls) ---
+# Simple listing with icons
+alias li='eza --icons'
+
+# Detailed listing
+alias ll='eza -lhmU --icons --git --total-size --time-style long-iso'
+
+# Detailed listing including hidden files
+alias la='eza -lahmU --icons --git --total-size --time-style long-iso'
+
+# Tree view
+alias lt='eza -a --tree --icons --no-git'
+# -----------------------
